1c62e019214ea0a67e6173ed3bc386006441f137
adrien sorgniard <a.sorgniard@gmail.com>
2018-06-19 23:49:32 +0200

    ligue 1 en crade

--- main.c
+++ main.c
@@ -2,14 +2,37 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 
 #define MUR  0
 #define PORTAIL 1
 #define VIDE 2
 
+#define NBR_TURN_LIGHT 3
+#define NBR_TURN_PLAN 5
+
 using namespace std;
 
-int MyExplorer = -1;
+int myExplorerId = -1;
+
+class Position
+{
+ public:
+  int x;
+  int y;
+
+  Position()
+    {
+      x = -1;
+      y = -1;
+    }
+
+  Position(int a, int b)
+    {
+      x = a;
+      y = b;
+    }
+};
 
 class Plateau
 {
@@ -18,6 +41,7 @@ class Plateau
   int height;
   int** plateau;
   int** monsterDistance;
+  vector<Position> cases;
 
   Plateau(const int a,const int b)
     {
@@ -25,50 +49,137 @@ class Plateau
       height = b;
       plateau = new int*[width];
       monsterDistance = new int*[width];
-      
+
       for(int i = 0; i < width; i++)
-	{
-	  plateau[i] = new int[height];
-	  monsterDistance = new int[height];
-	}
+        {
+          plateau[i] = new int[height];
+          monsterDistance[i] = new int[height];
+        }
     }
 
   void setPlateauLine(int index, string elements)
     {
       for(int i = 0; i < width; i++)
-	{
-	  if(elements[i] == '#')
-	    plateau[i][index] = MUR;
-	  else if (elements[i] == 'w')
-	    plateau[i][index] = PORTAIL;
-	  else if (elements[i] == '.')
-	    plateau[i][index] = VIDE;
-	  else
-	    cerr << "ERREUR description plateau : " << elements << " " << i << "\n";
-	}
+        {
+          if(elements[i] == '#')
+            plateau[i][index] = MUR;
+          else if (elements[i] == 'w')
+            plateau[i][index] = PORTAIL;
+          else if (elements[i] == '.')
+            plateau[i][index] = VIDE;
+          else
+            cerr << "ERREUR description plateau : " << elements << " " << i << "\n";
+        }
     }
+
+  int getPositionStatus(int x, int y)
+  {
+    if (x < 0 || x >= width)
+      return MUR;
+    if (y < 0 || y >= height)
+      return MUR;
+
+    return plateau[x][y];
+  }
+
+  void initMonsterDistance()
+  {
+    for (int i = 0; i < width; i++)
+      {
+        for (int j = 0; j < height; j++)
+          {
+            monsterDistance[i][j] = -1;
+          }
+
+      }
+
+    cases.clear();
+  }
+
+  void calculMonsterDistance()
+  {
+    vector<Position> aux;
+    int dist = 0;
+
+    while(cases.size() != 0)
+      {
+        for(int k = 0; k < cases.size(); k++)
+          {
+            int i = cases[k].x;
+            int j = cases[k].y;
+
+            if (monsterDistance[i][j] == -1)
+                monsterDistance[i][j] = dist;
+
+            if (getPositionStatus(i-1, j) == VIDE && monsterDistance[i-1][j] == -1)
+              aux.push_back(Position(i-1, j));
+            if (getPositionStatus(i+1, j) == VIDE && monsterDistance[i+1][j] == -1)
+              aux.push_back(Position(i+1, j));
+            if (getPositionStatus(i, j-1) == VIDE && monsterDistance[i][j-1] == -1)
+              aux.push_back(Position(i, j-1));
+            if (getPositionStatus(i, j+1) == VIDE && monsterDistance[i][j+1] == -1)
+              aux.push_back(Position(i, j+1));
+          }
+
+        dist++;
+        cases = aux;
+        aux.clear();
+      }    
+  }
+
+  Position getMaxDistanceMonster()
+  {
+    Position maxPos = Position(0,0);
+
+    for(int i = 0; i < width; i++)
+      {
+        for (int j = 0; j < height; j++)
+          {
+            if (monsterDistance[i][j] > monsterDistance[maxPos.x][maxPos.y])
+              {
+                maxPos = Position(i, j);
+              }
+          }
+      }
+
+    return maxPos;
+  }
+
+  Position getCloseMaxDistance(Position pos)
+  {
+    Position maxPos = pos;
+
+    if (getPositionStatus(pos.x-1, pos.y) == VIDE && monsterDistance[pos.x-1][pos.y] > monsterDistance[maxPos.x][maxPos.y])
+      maxPos = Position(pos.x-1, pos.y);
+    if (getPositionStatus(pos.x+1, pos.y) == VIDE && monsterDistance[pos.x+1][pos.y] > monsterDistance[maxPos.x][maxPos.y])
+      maxPos = Position(pos.x+1, pos.y);
+    if (getPositionStatus(pos.x, pos.y-1) == VIDE && monsterDistance[pos.x][pos.y-1] > monsterDistance[maxPos.x][maxPos.y])
+      maxPos = Position(pos.x, pos.y-1);
+    if (getPositionStatus(pos.x, pos.y+1) == VIDE && monsterDistance[pos.x][pos.y+1] > monsterDistance[maxPos.x][maxPos.y])
+      maxPos = Position(pos.x, pos.y+1);
+
+    return maxPos;
+  }
 };
 
 class Unit
 {
  public:
-  
+
   int id;
-  int x;
-  int y;
+  Position position;
 
   Unit()
     {
       id = 0;
-      x = 0;
-      y = 0;
+      position = Position();
     }
 
   Unit(int a, int b, int c)
     {
       id = a;
-      x = b;
-      y = c;
+      position.x = b;
+      position.y = c;
     }
 };
 
@@ -80,7 +191,9 @@ class Explorer : public Unit
 
   Explorer(int a, int b, int c, int d, int e, int f)
     {
-      Unit(a, b, c);
+      id = a;
+      position.x = b;
+      position.y = c;
       health = d;
     }
 };
@@ -94,7 +207,9 @@ class Wanderer : public Unit
 
   Wanderer(int a, int b, int c, int d, int e, int f)
     {
-      Unit(a, b, c);
+      id = a;
+      position.x = b;
+      position.y = c;
       invocation = d;
       state = e;
       idTarget = f;
@@ -103,6 +218,12 @@ class Wanderer : public Unit
 
 vector<Explorer> explorers;
 vector<Wanderer> wanderers;
+int nbr_use_light = 3;
+int nbr_use_plan = 2;
+int initial_life = -1;
+int nbr_turn_left_light = 0;
+int nbr_turn_left_plan = 0;
+
 
 Explorer getExplorerById(int id)
 {
@@ -123,6 +244,7 @@ int main()
     int height;
     cin >> height; cin.ignore();
     Plateau plateau(width, height);
+
     for (int i = 0; i < height; i++) {
         string line;
         getline(cin, line);
@@ -136,9 +258,17 @@ int main()
 
     // game loop
     while (1) {
-        int entityCount; // the first given entity corresponds to your explorer
+        plateau.initMonsterDistance();
+        myExplorerId = -1;
+        explorers.clear();
+        wanderers.clear();
+	nbr_turn_left_light = max(0, nbr_turn_left_light-1);
+	nbr_turn_left_plan = max(0, nbr_turn_left_plan-1);
+
+	int entityCount; // the first given entity corresponds to your explorer
         cin >> entityCount; cin.ignore();
-        for (int i = 0; i < entityCount; i++) {
+
+	for (int i = 0; i < entityCount; i++) {
             string entityType;
             int id;
             int x;
@@ -150,21 +280,61 @@ int main()
 
 	    if (entityType == "EXPLORER")
 	      {
-		if (MyExplorer == -1)
-		  MyExplorer = id;
-		explorers.push_back(Explorer(id, x, y, param0, param1, param2));
+    		if (myExplorerId == -1)
+    		{
+    		  cerr << "my explo : " << x << " " << y << endl;
+    		  myExplorerId = id;
+    		}
+		if (initial_life == -1)
+		  initial_life = param0;
+    		Explorer e = Explorer(id, x, y, param0, param1, param2);
+    		explorers.push_back(e);
 	      }
 	    else if(entityType == "WANDERER")
 	      {
-		wanderers.push_back(Wanderer(id, x, y, param0, param1, param2));
+    		Wanderer w = Wanderer(id, x, y, param0, param1, param2);
+    		wanderers.push_back(w);
+    		plateau.cases.push_back(w.position);
 	      }
 	    else
 	      cerr << "ERREUR lecture unit : " << entityType << "\n";
         }
 
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
+        cerr << myExplorerId << endl;
+        Explorer myExplorer = getExplorerById(myExplorerId);
+        cerr << "myExplorer position : " << myExplorer.position.x << " " << myExplorer.position.y << endl;
+        
+        plateau.calculMonsterDistance();
+        
+        if ( myExplorer.position.x != -1 && myExplorer.position.y != -1 &&
+	     plateau.monsterDistance[myExplorer.position.x][myExplorer.position.y] != -1)
+        {
+	    Position nextPos = plateau.getCloseMaxDistance(myExplorer.position);
 
-        cout << "WAIT" << endl; // MOVE <x> <y> | WAIT
+	    if (plateau.monsterDistance[nextPos.x][nextPos.y] <= 1 &&
+		nbr_turn_left_light == 0 &&
+		nbr_use_light > 0)
+	      {
+		nbr_use_light--;
+		nbr_turn_left_light = NBR_TURN_LIGHT;
+		cout << "LIGHT" << endl;
+	      }
+	    else if (myExplorer.health < initial_life / 2 &&
+		     nbr_turn_left_plan == 0 &&
+		     nbr_use_plan > 0)
+	      {
+		nbr_use_plan--;
+		nbr_turn_left_plan = NBR_TURN_PLAN;
+		cout << "PLAN" << endl;
+	      }
+	    else
+	      {
+		cout << "MOVE " << nextPos.x << " " << nextPos.y << endl; // MOVE <x> <y> | WAIT
+	      }
+        }
+        else
+        {
+            cout << "WAIT" << endl;
+        }
     }
 }
